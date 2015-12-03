7b4bbef1c37719f164b9380c39f1474b9daafa66
Yi Wang <yi.wang.2005@gmail.com>
2015-12-03 08:09:15 -0500

    Finish unit test

--- etcd_test.go
+++ etcd_test.go
@@ -1,6 +1,10 @@
 package etcd
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"testing"
+)
 
 func ExampleNew() {
 	flagEtcd := flag.String("etcd", "http://127.0.0.1:4001,http://127.0.0.1:2379", "Etcd peers addresses")
@@ -12,4 +16,47 @@ func ExampleNew() {
 	c.Get("/home/yi/a")
 	c.Get("home/yi/b")
 	c.Rmdir("/home")
+}
+
+var testEtcd = flag.String("testEtcd", "", "If non-empty, run tests.  An example is \"http://127.0.0.1:4001,http://127.0.0.1:2379\"")
+
+func TestEtcd(t *testing.T) {
+	if *testEtcd == "" {
+		fmt.Println("Test doesn't run without specifying testEtcd")
+		return
+	}
+
+	fmt.Println("Testing using etcd listening on ", *testEtcd)
+
+	if c, e := New(*testEtcd); e != nil {
+		t.Error(e)
+	} else {
+		c.Rmdir("/home/yi")
+		if e := c.Mkdir("/home/yi"); e != nil {
+			t.Error(e)
+		}
+		if e := c.Set("/home/yi/a", "Apple"); e != nil {
+			t.Error(e)
+		}
+		if e := c.Set("/home/yi/b", "Banana"); e != nil {
+			t.Error(e)
+		}
+		if r, e := c.Get("/home/yi/a"); e != nil || r != "Apple" {
+			t.Error(e)
+		}
+		if r, e := c.Get("home/yi/b"); e != nil || r != "Banana" {
+			t.Error(e)
+		}
+
+		if e := c.Set("/home/yi/a", "Aloha"); e != nil {
+			t.Error(e)
+		}
+		if r, e := c.Get("/home/yi/a"); e != nil || r != "Aloha" {
+			t.Error(e)
+		}
+
+		if e := c.Rmdir("/home"); e != nil {
+			t.Error(e)
+		}
+	}
 }
